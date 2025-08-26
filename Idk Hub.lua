1979881943346b71d832afa0deac86919b90bc00
Cauê.zx <caue61075@gmail.com>
2025-08-26 16:58:50 -0300

    Update Idk Hub.lua

--- Idk Hub.lua
+++ Idk Hub.lua
@@ -39,9 +39,3 @@ local Window = Rayfield:CreateWindow({
 }) 
 local PlayerTab = Window:CreateTab("Player", 4483362458) -- Title, Image
 
-local Button = Tab:CreateButton({
-   Name = "Button Example",
-   Callback = function()
-   -- The function that takes place when the button is pressed
-   end,
-})
